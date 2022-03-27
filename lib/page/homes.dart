49697c43c37b92b169140e8bf07b34830901da68
Navaphat Phiboon <phiboon_n@silpakorn.edu>
2022-03-27 09:33:13 +0700

    Initial Commit

---
+++ lib/page/homes.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    testConnectAPI();
+  }
+    Future <void> testConnectAPI() async {
+    Uri url = Uri.parse("https://cpsu-test-api.herokuapp.com/quizzes");
+    var response = await http.get(url, headers: {'id': '620710663'});
+
+    var json = jsonDecode(response.body);
+    if(json['status'] == 'ok'){
+     print('Connected Successful');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+
+
+}
